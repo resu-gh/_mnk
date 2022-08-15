a70d4ab2d1a1af245ff822aadb6a4f518dfc48a5
resu-gh <9valentino4@gmail.com>
2022-08-15 19:06:07 +0200

    eval functions (closure, high-order, first-class)

--- src/object/object.go
+++ src/object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"mnk/src/ast"
+	"strings"
+)
 
 type ObjectType string
 
@@ -8,8 +13,9 @@ const (
 	INTEGER_OBJ      = "INTEGER"
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
-	RETURN_VALUE_OBJ = "RETURN_VALUE_OBJ"
-	ERROR_OBJ        = "ERROR_OBJ"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type Object interface {
@@ -85,23 +91,32 @@ func (e *Error) Inspect() string {
 	return "ERROR: " + e.Message
 }
 
-// ============================== environment ir
+// ============================== function ir
 
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
-	return &Environment{store: s}
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
 }
 
-type Environment struct {
-	store map[string]Object
+func (f *Function) Type() ObjectType {
+	return FUNCTION_OBJ
 }
 
-func (e *Environment) Get(name string) (Object, bool) {
-	obj, ok := e.store[name]
-	return obj, ok
-}
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
 
-func (e *Environment) Set(name string, val Object) Object {
-	e.store[name] = val
-	return val
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
 }
