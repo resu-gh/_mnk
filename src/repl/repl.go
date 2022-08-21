83dbe4318c593fce0007c7f584369407d5cb20eb
resu-gh <9valentino4@gmail.com>
2022-08-21 15:08:57 +0200

    repl w/ vm and compiler

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -4,17 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"mnk/src/evaluator"
+	"mnk/src/compiler"
+	// "mnk/src/evaluator"
 	"mnk/src/lexer"
-	"mnk/src/object"
+	// "mnk/src/object"
 	"mnk/src/parser"
+	"mnk/src/vm"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-    env := object.NewEnvironment()
+	// env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -33,11 +35,29 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		// evaluated := evaluator.Eval(program, env)
+		// if evaluated != nil {
+		// 	io.WriteString(out, evaluated.Inspect())
+		// 	io.WriteString(out, "\n")
+		// }
+
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "woops! compilation failed:\n %s\n", err)
+			continue
 		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "woops! executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 
