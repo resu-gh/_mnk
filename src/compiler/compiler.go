5f1260fbeb41d49a25567e04431fd6e7afd14a44
resu-gh <9valentino4@gmail.com>
2022-12-30 15:40:14 +0100

    add object.String compiler string and concatenation + operator

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -165,6 +165,9 @@ func (c *Compiler) Compile(node ast.Node) error {
 		} else {
 			c.emit(code.OpFalse)
 		}
+	case *ast.StringLiteral:
+		str := &object.String{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(str))
 	}
 	return nil
 }
--- src/compiler/helper.go
+++ src/compiler/helper.go
@@ -60,6 +60,17 @@ func testIntegerObject(expected int64, actual object.Object) error {
 	return nil
 }
 
+func testStringObject(expected string, actual object.Object) error {
+	result, ok := actual.(*object.String)
+	if !ok {
+		return fmt.Errorf("object is not String. got=%T (%+v)", actual, actual)
+	}
+	if result.Value != expected {
+		return fmt.Errorf("object haw wrong value. got=%q, want=%q", result.Value, expected)
+	}
+	return nil
+}
+
 func testConstants(t *testing.T, expected []interface{}, actual []object.Object) error {
 	if len(expected) != len(actual) {
 		return fmt.Errorf("wrong number of constants. got=%d, want=%d", len(actual), len(expected))
@@ -71,6 +82,11 @@ func testConstants(t *testing.T, expected []interface{}, actual []object.Object)
 			err := testIntegerObject(int64(constant), actual[i])
 			if err != nil {
 				return fmt.Errorf("constant %d - testIntegerObject failed: %s", i, err)
+			}
+		case string:
+			err := testStringObject(constant, actual[i])
+			if err != nil {
+				return fmt.Errorf("constant %d - testStringObject failed: %s", i, err)
 			}
 		}
 	}
